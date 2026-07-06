9ab72f5783209f316505e41cf72ba761fe30582b
raven <agbosbenoit200507@gmail.com>
2026-07-06 15:32:09 +0000

    feat: customize UserAdmin to display company and role fields

--- backend/apps/accounts/admin.py
+++ backend/apps/accounts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from .models import User
 
 # Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    fieldsets = BaseUserAdmin.fieldsets + (
+        ("Informations NexHR", {"fields": ("company", "role")}),
+    )
+    
+    list_display = ("email", "username", "role", "company", "is_active")
+
+admin.site.register(User, UserAdmin)
