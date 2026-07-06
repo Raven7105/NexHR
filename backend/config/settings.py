09d15d07cbee78f67a63e0e30af3bbeb2de1eb27
raven <agbosbenoit200507@gmail.com>
2026-07-06 11:59:12 +0000

    feat: add custom User model with role field and configure AUTH_USER_MODEL

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -41,7 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.accounts',
 ]
+
+AUTH_USER_MODEL = 'accounts.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
