670f20e568fe0b0ff6147c1511e9982fbda8613e
raven <agbosbenoit200507@gmail.com>
2026-07-13 12:28:06 +0000

    feat: add Department and Employee API endpoints

--- backend/apps/employees/views.py
+++ backend/apps/employees/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+from rest_framework import viewsets
+from .serializers import DepartmentSerializer, EmployeeSerializer
+from .models import Department, Employee
 # Create your views here.
+
+
+class DepartmentViewSet(viewsets.ModelViewSet):
+    queryset = Department.objects.all()
+    serializer_class = DepartmentSerializer
+
+
+class EmployeeViewSet(viewsets.ModelViewSet):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
